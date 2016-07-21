e43c32df71a923aaea3a95fddafd359ca4a7c0ba
tiagofumo <tiagofumo@gmail.com>
2016-07-20 23:39:50 -0300

    add recursive version to kata02

--- kata02_karate_chop.rb
+++ kata02_karate_chop.rb
@@ -23,6 +23,19 @@ def chop(search, array)
   end
 end
 
+def chop_recursive(search, array, a = 0, b = array.length - 1)
+  c = (a + b) / 2
+  if a > b
+    -1
+  elsif array[c] == search
+    c
+  elsif array[c] > search
+    chop_recursive(search, array, a, c - 1)
+  else
+    chop_recursive(search, array, c + 1, b)
+  end
+end
+
 class TestChop < Test::Unit::TestCase
   def test_chop
     assert_equal(-1, chop(3, []))
@@ -46,5 +59,29 @@ class TestChop < Test::Unit::TestCase
     assert_equal(-1, chop(4, [1, 3, 5, 7]))
     assert_equal(-1, chop(6, [1, 3, 5, 7]))
     assert_equal(-1, chop(8, [1, 3, 5, 7]))
+  end
+
+  def test_chop_recursive
+    assert_equal(-1, chop_recursive(3, []))
+    assert_equal(-1, chop_recursive(3, [1]))
+    assert_equal(0,  chop_recursive(1, [1]))
+    #
+    assert_equal(0,  chop_recursive(1, [1, 3, 5]))
+    assert_equal(1,  chop_recursive(3, [1, 3, 5]))
+    assert_equal(2,  chop_recursive(5, [1, 3, 5]))
+    assert_equal(-1, chop_recursive(0, [1, 3, 5]))
+    assert_equal(-1, chop_recursive(2, [1, 3, 5]))
+    assert_equal(-1, chop_recursive(4, [1, 3, 5]))
+    assert_equal(-1, chop_recursive(6, [1, 3, 5]))
+    # #
+    assert_equal(0,  chop_recursive(1, [1, 3, 5, 7]))
+    assert_equal(1,  chop_recursive(3, [1, 3, 5, 7]))
+    assert_equal(2,  chop_recursive(5, [1, 3, 5, 7]))
+    assert_equal(3,  chop_recursive(7, [1, 3, 5, 7]))
+    assert_equal(-1, chop_recursive(0, [1, 3, 5, 7]))
+    assert_equal(-1, chop_recursive(2, [1, 3, 5, 7]))
+    assert_equal(-1, chop_recursive(4, [1, 3, 5, 7]))
+    assert_equal(-1, chop_recursive(6, [1, 3, 5, 7]))
+    assert_equal(-1, chop_recursive(8, [1, 3, 5, 7]))
   end
 end
