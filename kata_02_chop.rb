f2fc47b8e1704bcaaa57c846338c96b3cc6c7312
tiagofumo <tiagofumo@gmail.com>
2016-07-20 23:09:18 -0300

    add kata 02

---
+++ kata_02_chop.rb
@@ -0,0 +1,44 @@
+require "test/unit"
+
+def chop(search, array)
+  a = 0
+  b = max = array.length - 1
+  while true
+    c = (a + b) / 2
+    if a > max || b < 0 || a > b
+      return -1
+    elsif array[c] == search
+      return c
+    elsif array[c] > search
+      b = c - 1
+    else
+      a = c + 1
+    end
+  end
+end
+
+class TestChop < Test::Unit::TestCase
+  def test_chop
+    assert_equal(-1, chop(3, []))
+    assert_equal(-1, chop(3, [1]))
+    assert_equal(0,  chop(1, [1]))
+    #
+    assert_equal(0,  chop(1, [1, 3, 5]))
+    assert_equal(1,  chop(3, [1, 3, 5]))
+    assert_equal(2,  chop(5, [1, 3, 5]))
+    assert_equal(-1, chop(0, [1, 3, 5]))
+    assert_equal(-1, chop(2, [1, 3, 5]))
+    assert_equal(-1, chop(4, [1, 3, 5]))
+    assert_equal(-1, chop(6, [1, 3, 5]))
+    # #
+    assert_equal(0,  chop(1, [1, 3, 5, 7]))
+    assert_equal(1,  chop(3, [1, 3, 5, 7]))
+    assert_equal(2,  chop(5, [1, 3, 5, 7]))
+    assert_equal(3,  chop(7, [1, 3, 5, 7]))
+    assert_equal(-1, chop(0, [1, 3, 5, 7]))
+    assert_equal(-1, chop(2, [1, 3, 5, 7]))
+    assert_equal(-1, chop(4, [1, 3, 5, 7]))
+    assert_equal(-1, chop(6, [1, 3, 5, 7]))
+    assert_equal(-1, chop(8, [1, 3, 5, 7]))
+  end
+end
